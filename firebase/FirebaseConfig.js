e3ca10bdae83beb12e28cbe73ca2d90d044df38f
Shafiulla Attar <96431513+shafi78@users.noreply.github.com>
2023-08-17 23:54:24 +0530

    Update FirebaseConfig.js

--- firebase/FirebaseConfig.js
+++ firebase/FirebaseConfig.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYLhy20HbUXlVnq1IUrZiDpNw-72wavhk",
-  authDomain: "qnas-802e2.firebaseapp.com",
-  projectId: "qnas-802e2",
-  storageBucket: "qnas-802e2.appspot.com",
-  messagingSenderId: "822516087093",
-  appId: "1:822516087093:web:2623fd208564b0cb4ab47c",
-  measurementId: "G-XLCXKYQYRX"
+  apiKey: ""
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 if (!firebase.apps.length){
